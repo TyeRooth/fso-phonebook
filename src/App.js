26e35b0b26a749ad165480b62272075b44025c85
Tye Rooth <tyerooth@gmail.com>
2023-08-30 06:50:53 -0400

    Completed exercise 2.11. Persons retrieved from json-server

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+import Filter from "./components/Filter";
+import PersonForm from "./components/PersonForm";
+import Persons from "./components/Persons";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      setPersons(response.data);
+    });
+  }, []);
 
   const handleSearchInputChange = (event) => {
     setSearch(event.target.value);
@@ -56,33 +62,20 @@ const App = () => {
   return (
     <div>
       <h1>Phonebook</h1>
-      <div>
-        Filter shown with:
-        <input value={search} onChange={(e) => handleSearchInputChange(e)} />
-      </div>
+      <Filter
+        search={search}
+        handleSearchInputChange={handleSearchInputChange}
+      />
       <h2>Add a new person</h2>
-      <form onSubmit={(e) => addNewPerson(e)}>
-        <div>
-          name:{" "}
-          <input value={newName} onChange={(e) => handleNameInputChange(e)} />
-        </div>
-        <div>
-          number:{" "}
-          <input
-            value={newNumber}
-            onChange={(e) => handleNumberInputChange(e)}
-          />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <PersonForm
+        newName={newName}
+        handleNameInputChange={handleNameInputChange}
+        newNumber={newNumber}
+        handleNumberInputChange={handleNumberInputChange}
+        addNewPerson={addNewPerson}
+      />
       <h2>Numbers</h2>
-      {filteredPersons.map((person) => (
-        <p key={person.id}>
-          {person.name} {person.phone}
-        </p>
-      ))}
+      <Persons filteredPersons={filteredPersons} />
     </div>
   );
 };
