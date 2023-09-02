e9a671b260d49e4325bce6c9628d2bac15d8029e
Tye Rooth <tyerooth@gmail.com>
2023-09-02 09:26:13 -0400

    Completed exercise 2.14. Person can be deleted

--- src/components/Persons.js
+++ src/components/Persons.js
@@ -1,13 +1,14 @@
-const Persons = ({ filteredPersons }) => {
+const Persons = ({ filteredPersons, handlePersonDelete }) => {
   return (
     <div>
       {filteredPersons.map((person) => (
         <p key={person.id}>
-          {person.name} {person.phone}
+          {person.name} {person.phone}{" "}
+          <button onClick={() => handlePersonDelete(person.id)}>Delete</button>
         </p>
       ))}
     </div>
-  )
+  );
 };
 
 export default Persons;
