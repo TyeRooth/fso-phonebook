3479164ddf3ed2bd83eabef97b9d4204993008f8
Tye Rooth <tyerooth@gmail.com>
2023-09-02 09:53:38 -0400

    Completed exercises 2.14 and 2.15.  Delete and update numbers.

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ const App = () => {
       .then((initialPersons) => {
         setPersons(initialPersons);
       })
-      .catch(() => alert(`Failed to get persons.  Please refresh page.`));
+      .catch((error) => alert(`Failed to get persons.  Please refresh page.`));
   }, []);
 
   const handleSearchInputChange = (event) => {
@@ -37,16 +37,39 @@ const App = () => {
     return persons.find((person) => person.name === newName);
   };
 
+  const updatePhoneForPerson = (newPerson) => {
+    if (
+      window.confirm(
+        `${newPerson.name} is already added to the phonebook, replace the old number with a new one?`
+      )
+    ) {
+      const personToUpdate = persons.find((person) => person.name === newName);
+      const updatedPerson = { ...personToUpdate, phone: newNumber };
+      personsService
+        .updatePhone(personToUpdate.id, updatedPerson)
+        .then((updatedPerson) => {
+          setPersons(
+            persons.map((person) => {
+              return person.id !== updatedPerson.id ? person : updatedPerson;
+            })
+          );
+          setNewName("");
+          setNewNumber("");
+        })
+        .catch((error) => alert(`Failed to update number for person.`));
+    }
+  };
+
   const addNewPerson = (e) => {
     e.preventDefault();
-    if (nameInPhonebook()) {
-      alert(`${newName} is already added to phonebook`);
-      return;
-    }
     const newPerson = {
       name: newName,
       phone: newNumber,
     };
+    if (nameInPhonebook()) {
+      updatePhoneForPerson(newPerson)
+      return
+    }
     personsService
       .addPerson(newPerson)
       .then((postedPerson) => {
@@ -54,7 +77,21 @@ const App = () => {
         setNewName("");
         setNewNumber("");
       })
-      .catch(() => alert(`Failed to add person.  Please try again.`))
+      .catch((error) => alert(`Failed to add person.  Please try again.`));
+  };
+
+  const handlePersonDelete = (id) => {
+    const personToDelete = persons.find((person) => person.id === id);
+    if (window.confirm(`Delete ${personToDelete.name}`)) {
+      personsService
+        .deletePerson(id)
+        .then(() => {
+          setPersons(persons.filter((person) => person.id !== id));
+        })
+        .catch((error) => {
+          alert(`Failed to delete person.  Please try again.`);
+        });
+    }
   };
 
   const allSearchCharsInName = (name) => {
@@ -84,7 +121,10 @@ const App = () => {
         addNewPerson={addNewPerson}
       />
       <h2>Numbers</h2>
-      <Persons filteredPersons={filteredPersons} />
+      <Persons
+        filteredPersons={filteredPersons}
+        handlePersonDelete={handlePersonDelete}
+      />
     </div>
   );
 };
--- src/services/persons.js
+++ src/services/persons.js
@@ -12,9 +12,21 @@ const addPerson = async (newPerson) => {
   return response.data;
 };
 
+const deletePerson = async (id) => {
+  const response = await axios.delete(`${baseUrl}/${id}`);
+  return response.data;
+};
+
+const updatePhone = async (id, updatedPerson) => {
+  const response = await axios.put(`${baseUrl}/${id}`, updatedPerson);
+  return response.data;
+};
+
 const exportedFunctions = {
   getPersons,
   addPerson,
+  deletePerson,
+  updatePhone,
 };
 
 export default exportedFunctions;
